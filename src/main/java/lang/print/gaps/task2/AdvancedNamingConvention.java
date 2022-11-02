3c10b8002de46fd028a003eee80304f3ff4ac389
Islombek Yadgarov <islam.yadgarov.677@gmail.com>
2022-11-02 11:57:34 +0500

    my commit

--- src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
@@ -1,16 +1,16 @@
 package lang.print.gaps.task2;
 
-public class advancedNamingConvention {
+public class AdvancedNamingConvention {
     private static final int ADULT_AGE = 18;
 
     private int age;
     private int phone_number;
 
-    void callToFriend() {
+    void call_to_friend() {
         //some code
     }
 
-    void callByNumber(int Number) {
+    void call_by_number(int Number) {
         //some code
     }
 }
