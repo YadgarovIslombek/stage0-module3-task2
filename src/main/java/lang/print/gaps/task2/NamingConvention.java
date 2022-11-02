d204e8eaf10011a6923db057a1a356d73d958fd8
Islombek Yadgarov <islam.yadgarov.677@gmail.com>
2022-11-02 12:04:25 +0500

    my commit

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -2,14 +2,15 @@ package lang.print.gaps.task2;
 
 public class NamingConvention {
     public static void main(String[] args) {
-        int variable = 10;
-        int variable_One = 1;
-        int variable_Two = 2;
-        int variable_three = 3;
         final int VARIABLE_FOUR = 4;
         final int VARIABLE_FIVE = 5;
-        int variable_Six = 6;
+
+        int variable_one = 1;
+        int variable_two = 2;
+        int variable_three = 3;
+        int variable_six = 6;
         int variable_seven = 7;
         int variable_eight = 8;
+        int variable = 10;
     }
 }
